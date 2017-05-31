e5e6c64f999a030d12542571f52b084c95024446
Shaun <seyyes2000@hotmail.com>
2017-05-31 11:28:49 -0400

    add floats and rounding

--- minus.py
+++ minus.py
@@ -5,5 +5,5 @@ x2 = float(input("\nEnter x of the second vector: "))
 y2 = float(input("Enter y of the second vector: "))
 
 print ("\nThe sum of the vectors is: ")
-print ("[",round(x1+x2, 3),"]")
-print ("[",round(y1+y2, 3),"]")
+print ("[",round(x1-x2, 3),"]")
+print ("[",round(y1-y2, 3),"]")
