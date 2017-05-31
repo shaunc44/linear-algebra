799090dbef20435f12874004d8ec97a5b2dec5f0
Shaun <seyyes2000@hotmail.com>
2017-05-31 11:36:18 -0400

    finish scalar multiply

---
+++ scalar_multiply.py
@@ -0,0 +1,10 @@
+scalar = float(input("\nEnter the scalar: "))
+
+x = float(input("\nEnter x of the vector: "))
+y = float(input("Enter y of the vector: "))
+z = float(input("Enter z of the vector: "))
+
+print ("\nThe scalar multiplied is: ")
+print ("[",round(scalar * x, 3),"]")
+print ("[",round(scalar * y, 3),"]")
+print ("[",round(scalar * z, 3),"]")
