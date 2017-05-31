07f6aee0723550f4f343c7a98d4a163df4b2be92
Shaun <seyyes2000@hotmail.com>
2017-05-31 11:15:18 -0400

    finish addition.py

---
+++ addition.py
@@ -0,0 +1,9 @@
+x1 = int(input("\nEnter x of the first vector: "))
+y1 = int(input("Enter y of the first vector: "))
+
+x2 = int(input("\nEnter x of the second vector: "))
+y2 = int(input("Enter y of the second vector: "))
+
+print ("\nThe sum of the vectors is: ")
+print ("[",x1+x2,"]")
+print ("[",y1+y2,"]")
